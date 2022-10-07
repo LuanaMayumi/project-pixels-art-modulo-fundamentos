0f1945c43b005bfe6238f21f93a4c2f6d2a84204
LuanaMayumi <luh.mayumi@gmail.com>
2022-10-07 11:12:33 -0300

    exercicio 9 resolvido

--- TESTE SCRIPT.js
+++ TESTE SCRIPT.js
@@ -28,63 +28,57 @@ paintingSquareBlueMarine ()
 
 
 
-function paintingSquare () {
-
-    
-    let aleatoryColors =['rgb(0, 0, 0)']; //guardando as cores aleatórias dentro do array
-   
-    let gettingClassColors = document.querySelectorAll('.color'); // capturando a classe COLOR dos quadrados
-    gettingClassColors[0].style.backgroundColor = 'black'
-        
-    for (let index = 1; index < gettingClassColors.length ; index += 1) {
-
-    
-         gettingClassColors[index].style.backgroundColor = gettingColors(); 
-
-    aleatoryColors.push(gettingClassColors[index])//subir cores aleatorias para meu array
-
-        return aleatoryColors
-    }
-
-      function addLocalStorage (arrayColors) { //// criar uma função que vai receber um parametro e o que for recebido no parametro vai ser mandado para o localStorage,pra criar (set item)
-localStorage.setItem('colorPalette', JSON.stringify(arrayColors));
-    }
-
-    function getLocalStorage () { // as coisas do local storage e outra pra pegar (get items)
-    const pegandoItem =  localStorage.getItem('colorPalette')
-    }
-
-    botaoReset.addEventListener('click', function (){
-        addLocalStorage(paintingSquare())
-        adicionacor();
-        
-    })
-
-window.onload = function () {
-getLocalStorage()//recuperando os dados
-
-}
-
 
 
 //exercicio 9 pra pegar a cor
 
-function seletingColor () {
-let capturing = document//capturar o pixelpegar a classe
+// 1 fzer um addEventLitener para que qdo eu clico na cor eu pego o selected
+// 2 percorrer um for pra ver se alguma das minhas cores tem a classe seleted
+// Se sim, copiar o meu RGB pro click/selected
+// 3 clicar no grid e colar o meu rgb
+let button = document.getElementById('button-random-color')
 
-// for (let index = 0; index < capturing.length, index += 1)
-capturing.addEventListener('click', function (event) {
-    console.log(event.target)
-    event.target.className = 'color selected'
-
-    if (className === selected)
+button.addEventListener('click', function(event){
+    let capturing = document.getElementsByClassName('selected') //capturando a classe Selected
+    let capturing = event.target.className; //capturando as classes
     
-   
+    
+
+
+})
+
+function changingColor () {
+    let capturingClassSelected = document.getElementsByClassName('selected') //pegando a classe Selected
+    let capturingClassColor = document.getElementsByClassName('color')
+    let capturingPalette = document.getElementById('color-palette') //pegando meu palette
+    let capturingPixels = document.getElementById('pixelSection') //pegando o ID do grid
+  
+
+    capturingPalette.addEventListener('click', function (event) {
+    
+        event.target.className = 'color selected' //capturando a Classe
+    
+    for (let index = 0; index < capturingPalette.length; index += 1)
+    
+        if (capturingPalette[index] === capturingClassSelected) { //precisa por algo pra className for igual a selected? //SE algum quadrado do palette tiver a classe Slected
+
+            capturingPixels.style.backgroundColor = capturingClassSelected
+        }
+        else {
+            capturingPixels.style.backgroundColor = capturingClassColor
+        }
+    
+    })
 }
 
 
 
-}
+
+
+
+
+
+
 
 function selectColor(){
     let treinador = "black"
@@ -97,28 +91,10 @@ function selectColor(){
     quadrado1[0].addEventListener('click', function(event) {
         quadrado1[0].style.backgroundColor = treinador
     })
-   // }
 
 
-//exercicio 9 > a captura do botão deve ficar depois que clicamos no botao para cor aleatorias ?
-window.onload =function () {
-let button = document.getElementById('#button-random-color');
-//criando uma variável pra pegar o botão
-
-}
-button.addEventListener > para clicar e pegar a cor 
 
 
-function xxx
-se clicar na cor, guardar a cor
-
-outro button pra clicar e deixar a cor?
-
-//    let EscolherACor = tentar pegar a cor aqui
-
-   if (EscolherACor) {
-
-   }
 
 //    Crie uma função para selecionar uma cor na paleta de cores e preencha os pixels no quadro.
 //    A cor clicada deve ser selecionada e poderá ser utilizada para preencher os quadros
--- script.js
+++ script.js
@@ -4,70 +4,70 @@ let criandoUl = document.createElement('ul')
 criandoUl.id = 'color-palette'
 sectionPai.appendChild(criandoUl)
 
-function addingPaletaCores () {
+function addingPaletaCores() {
 
-for (let index = 0; index < 4; index += 1) {
-let criandoLi = document.createElement('li')
-criandoLi.className = 'color'
-criandoUl.appendChild(criandoLi)
-}
-}
-
-function addClasseSelected () {
-let pegandoClasseBlack = document.getElementsByClassName('color')[0];
-pegandoClasseBlack.classList.add('selected'); 
-}
-
-
-function gettingColors () {
-let r = Math.floor(Math.random() * 255);
-let g = Math.floor(Math.random() * 255);
-let b = Math.floor(Math.random() * 255);
-return rgb = `rgb(${r}, ${g}, ${b})`;
-}
-
-function addLocalStorage (arrayColors) { //adicionando cores novas no local storage, depois que a função é chamada no PantingSquare (linha 48)
-localStorage.setItem('colorPalette', JSON.stringify(arrayColors)); 
-}
-
-
-function paintingSquare () {
-  
-let aleatoryColors =['rgb(0, 0, 0)']; 
-let gettingClassColors = document.getElementsByClassName('color'); // capturando os quadrados
-    
-console.log(gettingClassColors)
-gettingClassColors[0].style.backgroundColor = 'black' //colocando cor preta no 1 quadrado
-    
-        
-for (let index = 1; index < gettingClassColors.length ; index += 1) { 
-gettingClassColors[index].style.backgroundColor = gettingColors();       
-aleatoryColors.push(gettingClassColors[index].style.backgroundColor) //pintando outros quadrados com cor aleatoria
-        
-}
-addLocalStorage(aleatoryColors) //toda vez que uma cor é gerada esta sendo salva no LocalStorage
-}
-
-function getLocalStorage () {  //pegando as cores do local Storage // qundo eu fecho o nagevador, vai puxar minhas cores iniciais e o localStorage vai estar vazio. 
-const pegandoItem = JSON.parse(localStorage.getItem('colorPalette')) 
-console.log(pegandoItem)
-recebeCoresPintaLocalStorage (pegandoItem) //pegando item ColorPalette do localStorage com as cores, e atribuindo pra função RecebeCoresPintaLocalStorage que vai pintas os quadrados, embaixo
+    for (let index = 0; index < 4; index += 1) {
+        let criandoLi = document.createElement('li')
+        criandoLi.className = 'color'
+        criandoUl.appendChild(criandoLi)
     }
-        
-
-
-function recebeCoresPintaLocalStorage (coresLocalStorage){ //rececendo as cores salvas no LocalStorage
-
-if (coresLocalStorage !== null) { // se as cores salvas lo LocalStorage forem diferente de Nulas (se tiver cor)
-
-let gettingClassColors = document.getElementsByClassName('color'); // capturando os quadrados 
-
-    for (let index = 0; index < coresLocalStorage.length ; index += 1) { //percorrendo as cores que estao no localStorage
-
-        gettingClassColors[index].style.backgroundColor = coresLocalStorage[index]; //adicionando as cores aos quadrados    
-        }
 }
-}     
+
+function addClasseSelected() {
+    let pegandoClasseBlack = document.getElementsByClassName('color')[0];
+    pegandoClasseBlack.classList.add('selected');
+}
+
+
+function gettingColors() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return rgb = `rgb(${r}, ${g}, ${b})`;
+}
+
+function addLocalStorage(arrayColors) { //adicionando cores novas no local storage, depois que a função é chamada no PantingSquare (linha 48)
+    localStorage.setItem('colorPalette', JSON.stringify(arrayColors));
+}
+
+
+function paintingSquare() {
+
+    let aleatoryColors = ['rgb(0, 0, 0)'];
+    let gettingClassColors = document.getElementsByClassName('color'); // capturando os quadrados
+
+    console.log(gettingClassColors)
+    gettingClassColors[0].style.backgroundColor = 'black' //colocando cor preta no 1 quadrado
+
+
+    for (let index = 1; index < gettingClassColors.length; index += 1) {
+        gettingClassColors[index].style.backgroundColor = gettingColors();
+        aleatoryColors.push(gettingClassColors[index].style.backgroundColor) //pintando outros quadrados com cor aleatoria
+
+    }
+    addLocalStorage(aleatoryColors) //toda vez que uma cor é gerada esta sendo salva no LocalStorage
+}
+
+function getLocalStorage() {  //pegando as cores do local Storage // qundo eu fecho o nagevador, vai puxar minhas cores iniciais e o localStorage vai estar vazio. 
+    const pegandoItem = JSON.parse(localStorage.getItem('colorPalette'))
+    console.log(pegandoItem)
+    recebeCoresPintaLocalStorage(pegandoItem) //pegando item ColorPalette do localStorage com as cores, e atribuindo pra função RecebeCoresPintaLocalStorage que vai pintas os quadrados, embaixo
+}
+
+
+
+function recebeCoresPintaLocalStorage(coresLocalStorage) { //rececendo as cores salvas no LocalStorage
+
+    if (coresLocalStorage !== null) { // se as cores salvas lo LocalStorage forem diferente de Nulas (se tiver cor)
+
+        let gettingClassColors = document.getElementsByClassName('color'); // capturando os quadrados 
+
+        for (let index = 0; index < coresLocalStorage.length; index += 1) { //percorrendo as cores que estao no localStorage
+
+            gettingClassColors[index].style.backgroundColor = coresLocalStorage[index]; //adicionando as cores aos quadrados    
+        }
+    }
+}
 
 
 let gettingButton = document.getElementById('button-random-color') //aperta o botão pra receber cores aleatórias
@@ -78,51 +78,78 @@ gettingButton.addEventListener('click', paintingSquare)
 let pixelSection = document.getElementById('pixelSection') //capturando o quadro Pixel
 
 
-for (let index = 0; index < 5 ; index += 1) { //criando o quadro através de UL
-let section = document.createElement('ul');
-section.id = 'pixel-board'
-pixelSection.appendChild(section);
+for (let index = 0; index < 5; index += 1) { //criando o quadro através de UL
+    let section = document.createElement('ul');
+    section.id = 'pixel-board'
+    pixelSection.appendChild(section);
 
-    for(let index2 = 0; index2 < 5; index2 += 1) { //criando o quadro através de li
-    let criandoLi = document.createElement('li');
-    criandoLi.classList.add('pixel') //para add uma classe sem sobreescrever a que existe
-    section.appendChild(criandoLi)
+    for (let index2 = 0; index2 < 5; index2 += 1) { //criando o quadro através de li
+        let criandoLi = document.createElement('li');
+        criandoLi.classList.add('pixel') //para add uma classe sem sobreescrever a que existe
+        section.appendChild(criandoLi)
+    }
 }
-}  
 
-function paintingSquareBlack (){
-let pintandoQuadrado0 = document.getElementsByClassName('color')
-pintandoQuadrado0[0].style.backgroundColor = 'black'
+function paintingSquareBlack() {
+    let pintandoQuadrado0 = document.getElementsByClassName('color')
+    pintandoQuadrado0[0].style.backgroundColor = 'black'
 }
+
+function paintingSquarePurple() {
+    let pintandoQuadrado1 = document.getElementsByClassName('color')
+    pintandoQuadrado1[1].style.backgroundColor = 'rgb(201,152,247)'
+}
+
+function paintingSquareBlue() {
+    let pintandoQuadrado2 = document.getElementsByClassName('color')
+    pintandoQuadrado2[2].style.backgroundColor = 'rgb(46,134,216)'
+}
+
+function paintingSquareBlueMarine() {
+    let pintandoQuadrado3 = document.getElementsByClassName('color')
+    pintandoQuadrado3[3].style.backgroundColor = 'rgb(14,104,187)'
+}
+
+//EXERCICIO 9
+
+let selected2 = document.getElementsByClassName('selected')
+console.log(selected2); //retorna em array!
+
+let paletteCores = document.getElementById('color-palette') //retorna em array?
+console.log(paletteCores);
+
+
+    paletteCores.addEventListener('click', function (event) {
+console.log('entrei');
+
+let selected = document.querySelector('.selected')
+console.log(selected); //está retornando Null
+  
+            selected.classList.remove('selected')
+            event.target.classList.add('selected')
+        }
+             );
+
+
+// paletteCores.addEventListener('click', function (event) {
+
+//     selected.classList.remove('selected')
+
+//     event.target.className = 'color selected';
     
-function paintingSquarePurple (){
-let pintandoQuadrado1 = document.getElementsByClassName('color')
-pintandoQuadrado1[1].style.backgroundColor = 'rgb(201,152,247)'
-}
-    
-function paintingSquareBlue (){
-let pintandoQuadrado2 = document.getElementsByClassName('color')
-pintandoQuadrado2[2].style.backgroundColor = 'rgb(46,134,216)'
-}
-    
-function paintingSquareBlueMarine () {
-let pintandoQuadrado3 = document.getElementsByClassName('color')
-pintandoQuadrado3[3].style.backgroundColor = 'rgb(14,104,187)'
-}
+
+ 
 
 
 
 window.onload = function () {
-paintingSquareBlack ()
-paintingSquarePurple ()
-paintingSquareBlue ()
-paintingSquareBlueMarine ()
-getLocalStorage ()
-
-}
+            paintingSquareBlack()
+            paintingSquarePurple()
+            paintingSquareBlue()
+            paintingSquareBlueMarine()
+            getLocalStorage()
+        }
 
 // chamadas functions
-addingPaletaCores ()
-addClasseSelected ()
-
-
+addingPaletaCores();
+addClasseSelected();
